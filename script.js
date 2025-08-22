50d773cac2ca7275f56c682f22416340d8a2c007
prashanthdotpm-lab <prashanthdotpm@gmail.com>
2025-08-22 23:31:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const forms = document.querySelectorAll("form");
+  forms.forEach(form => {
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+      alert("Form submitted successfully! (Demo only)");
+    });
+  });
+});
